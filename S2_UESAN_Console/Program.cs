d56b070700fd8b0cbbe8a62748c865d016cf7fb2
Luis Chang <luis.chang@outlook.com>
2021-08-31 08:58:14 -0500

    Agregar archivos de proyecto.

---
+++ S2_UESAN_Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using S2_UESAN_Console.Entidades;
+using System;
+
+namespace S2_UESAN_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string nombreCurso = "Desarrollo de Aplicaciones Web 2021-2";
+            int edad = 20;
+            double monto = 200.01;
+
+            var nuevaEdad = "Veintiuno";
+            var peso = 70.50;
+
+            var persona = new Persona();
+
+
+
+
+        }
+    }
+}
